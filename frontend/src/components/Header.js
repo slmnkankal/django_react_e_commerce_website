b732cfbba2be8934164416bce38b355e6dd51ec5
slmnkankal <slmnknkl@gmail.com>
2022-10-29 00:01:40 +0300

    HomeScreen.js and Product.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  Form,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function Header() {
   return (
@@ -18,8 +11,12 @@ function Header() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                <Nav.Link href="/cart">
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
