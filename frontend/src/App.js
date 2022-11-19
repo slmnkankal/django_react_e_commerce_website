220d18aa3a1935b4d2e20e9abb1dbe05ace9fe8b
slmnkankal <slmnknkl@gmail.com>
2022-11-19 15:35:39 +0300

    react router dom applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import { Container } from "react-bootstrap";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
@@ -7,16 +8,24 @@ function Header() {
       <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">E Commerce</Navbar.Brand>
+            <LinkContainer to="/">
+              <Navbar.Brand>E Commerce</Navbar.Brand>
+            </LinkContainer>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/cart">
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-                <Nav.Link href="/login">
-                  <i className="fas fa-user"></i>Login
-                </Nav.Link>
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <i className="fas fa-shopping-cart"></i> Cart
+                  </Nav.Link>
+                </LinkContainer>
+                
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i>Login
+                  </Nav.Link>
+                </LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Container>
