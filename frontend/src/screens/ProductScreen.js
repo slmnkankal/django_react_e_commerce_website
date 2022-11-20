3ed70847cddfc5ec4cd995a0564425b484f307da
slmnkankal <slmnknkl@gmail.com>
2022-11-20 17:03:46 +0300

    product details in ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,10 +6,64 @@ import products from '../products'
 
 function ProductScreen() {
   const { id } = useParams();
-  const product = products.find((p) => p._id === (id));
+  const product = products.find((p) => p._id === id);
   return (
     <div>
-      {product.name}
+      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button className='btn-block' disabled={product.countInStock === 0} type='button'>Add to Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
