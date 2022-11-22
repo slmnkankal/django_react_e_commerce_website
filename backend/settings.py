b673c64e3f56c375a3215f75e1a84e2ec3c788d5
slmnkankal <slmnknkl@gmail.com>
2022-11-22 10:50:47 +0300

    STATICFILES_DIRS, MEDIA_ROOT, MEDIA_URL

--- backend/settings.py
+++ backend/settings.py
@@ -123,10 +123,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/images/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
