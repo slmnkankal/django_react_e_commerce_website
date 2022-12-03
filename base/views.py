1dc8c08ccbe0ecb06dd0831055e3d2ba16aa07b3
slmnkankal <slmnknkl@gmail.com>
2022-12-03 21:39:52 +0300

    getRoutes is deleted

--- base/views.py
+++ base/views.py
@@ -27,23 +27,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products',
-        '/api/products/create/',
 
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    ]
-    return Response(routes)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
