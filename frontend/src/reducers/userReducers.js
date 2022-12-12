b53e31bdca8badc7f827339001f979b140008329
slmnkankal <slmnknkl@gmail.com>
2022-12-12 21:57:17 +0300

    userDetailsReducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,10 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = { }, action) => {
@@ -43,6 +47,23 @@ export const userRegisterReducer = (state = { }, action) => {
 
         case USER_LOGOUT:
             return {}
+
+        default:
+            return state
+    }
+}
+
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch(action.type){
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true}
+
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload}
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload}
 
         default:
             return state
