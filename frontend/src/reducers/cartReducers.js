d87cc3b906b6700cc463959a13c9354742e1f2d7
slmnkankal <slmnknkl@gmail.com>
2022-12-16 23:35:12 +0300

    related case has been added to the cartReducer

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,12 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { 
+  CART_ADD_ITEM, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_SHIPPING_ADDRESS,
 
-export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
+  CART_SAVE_PAYMENT_METHOD,
+ } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [], shippingAddress: {}, paymentMethod: {} }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -31,6 +37,13 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
         ...state,
         shippingAddress: action.payload
       }
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload
+      }
+
     default:
       return state;
   }
