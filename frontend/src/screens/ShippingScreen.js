885c885be5aa094cc154bf679efe98ba360eab87
slmnkankal <slmnknkl@gmail.com>
2022-12-16 14:17:11 +0300

    saveShippingAddress action is dispatched

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,16 +3,26 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from '../actions/cartActions';
 
 function ShippingScreen() {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+
+  const cart = useSelector(state => state.cart)
+  const { shippingAddress } = cart
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate()
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log('submitted...')
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    navigate('/payment')
   }
 
   return (
