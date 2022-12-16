10415ea3d084cc916408f40def35cf29d3246cbb
slmnkankal <slmnknkl@gmail.com>
2022-12-16 21:47:23 +0300

    CheckoutSteps in ShippingScreen with props

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from '../actions/cartActions';
 
 function ShippingScreen() {
@@ -27,6 +28,7 @@ function ShippingScreen() {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
