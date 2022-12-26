58b4e0ee847779588c18c1bf5c4dc90134a50121
slmnkankal <slmnknkl@gmail.com>
2022-12-26 22:33:39 +0300

    constant & reducer for CART_CLEAR_ITEMS

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,6 +4,8 @@ import {
   CART_SAVE_SHIPPING_ADDRESS,
 
   CART_SAVE_PAYMENT_METHOD,
+
+  CART_CLEAR_ITEMS,
  } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {}, paymentMethod: {} }, action) => {
@@ -42,6 +44,12 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {}, paymen
       return {
         ...state,
         paymentMethod: action.payload
+      }
+
+    case CART_CLEAR_ITEMS:
+      return {
+        ...state,
+        cartItems: []
       }
 
     default:
