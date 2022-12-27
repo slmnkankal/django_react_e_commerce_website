ece39e404842a35defc4b62a6c2cb5f0d0037f55
slmnkankal <slmnknkl@gmail.com>
2022-12-27 15:01:00 +0300

    review for useEffect

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -26,7 +26,7 @@ function OrderScreen() {
     if (!order || order._id !== Number(orderId)) {
       dispatch(getOrderDetails(orderId));
     }
-  }, [order, orderId]);
+  }, [dispatch, order, orderId]);
 
   return loading ? (
     <Loader />
