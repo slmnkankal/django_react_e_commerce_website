2741bef7e108ad940e81d96a431426f8b39ccbe2
slmnkankal <slmnknkl@gmail.com>
2022-12-28 21:32:44 +0300

    getMyOrders view

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -57,6 +57,16 @@ def addOrderItems(request):
         serializer=OrderItemSerializer(order, many=False)
         return Response(serializer.data)
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+    
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
