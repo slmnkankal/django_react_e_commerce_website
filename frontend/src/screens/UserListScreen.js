a93a4a00a44ffb00df6e8246a582fc2444291279
slmnkankal <slmnknkl@gmail.com>
2022-12-29 14:14:41 +0300

    users table is created with map function

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,40 +5,68 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listUsers } from '../actions/userActions';
+import { listUsers } from "../actions/userActions";
 
 function UserListScreen() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
 
-    const userList = useSelector(state => state.userList)
-    const { loading, error, users } = userList
-
-    useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
   return (
     <div>
-        <h1>Users</h1>
-        {loading
-          ? (<Loader />)
-          : error
-            ? (<Message variant='danger'>{error}</Message>)
-            : (
-                <Table striped bordered hover responsive className="table-sm">
-                  <thead>
-                    <th>ID</th>
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>ADMIN</th>
-                    <th></th>
-                  </thead>
-
-                </Table>
-            )
-          }
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th>EDIT</th>
+              <th>DELETE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className="fas fa-check" style={{ color: 'green' }}></i>
+                  ) : (
+                    <i className="fas fa-check" style={{ color: 'red'}}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/admin/user/${user._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit" style={{ color:'orange' }}></i>
+                    </Button>
+                  </LinkContainer>
+                </td>
+                <td>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-trash" style={{ color:'red' }}></i>
+                    </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
-  )
+  );
 }
 
-export default UserListScreen
+export default UserListScreen;
