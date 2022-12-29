697c5da720cef38ab52f15df21b674fb8c412a5b
slmnkankal <slmnknkl@gmail.com>
2022-12-29 10:49:02 +0300

    route for UserListScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   return (
@@ -32,6 +33,8 @@ function App() {
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
+
+            <Route path="/admin/userlist/" element={<UserListScreen />} />
           </Routes>
         </Container>
       </main>
