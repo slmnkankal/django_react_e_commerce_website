aafacba7d957ac2588df1bd03d13e96295208753
slmnkankal <slmnknkl@gmail.com>
2022-12-29 16:47:17 +0300

    deleteUser() was dispatched in deleteHandler()

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,7 +5,7 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
 function UserListScreen() {
   const dispatch = useDispatch();
@@ -18,16 +18,19 @@ function UserListScreen() {
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
+  const userDelete = useSelector(state => state.userDelete)
+  const { success:successDelete } = userDelete
+
   useEffect(() => {
     if(userInfo && userInfo.isAdmin){
       dispatch(listUsers());
     } else {
       navigate('/login')
     }
-  }, [dispatch, navigate]);
+  }, [dispatch, navigate, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log('DELETE:', id)
+    dispatch(deleteUser(id))
   }
 
   return (
