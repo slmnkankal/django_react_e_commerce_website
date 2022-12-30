2a0813116f81bdc7ae858681fa7ebe63d4c0f036
slmnkankal <slmnknkl@gmail.com>
2022-12-30 09:32:12 +0300

    url for getUserById

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('profile/update/', views.updateUserProfile, name="user-profile-update" ),
     path('', views.getUsers, name="users" ),
 
+    path('<str:pk>/', views.getUserById, name='user'),
+
     path('update/<str:pk>/', views.updateUser, name='user-update'),
 
     path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
