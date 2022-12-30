0c8566cf774c7a99acee6f146f0b0429fa295775
slmnkankal <slmnknkl@gmail.com>
2022-12-30 20:44:33 +0300

    to fill current user's info not the updated user's

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -36,7 +36,7 @@ function ProfileScreen() {
     if (!userInfo) {
       navigate("/login");
     } else {
-      if (!user || !user.name || success ) {
+      if (!user || !user.name || success || userInfo._id !== user._id) {
         dispatch({type: USER_UPDATE_PROFILE_RESET})
         dispatch(getUserDetails("profile"));
         dispatch(listMyOrders())
