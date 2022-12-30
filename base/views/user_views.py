757d961b64ae20d58368ba87e14fe53a57eedc89
slmnkankal <slmnknkl@gmail.com>
2022-12-30 09:26:48 +0300

    updateUser view

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -89,6 +89,25 @@ def getUserById(request, pk):
     return Response(serializer.data)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+
+    data = request.data
+
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+    
+    user.save()
+
+    serializer = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
+
+
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def deleteUser(request, pk):
