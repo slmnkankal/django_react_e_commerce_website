908588c7e68717e912729f902a172a4c46af1799
slmnkankal <slmnknkl@gmail.com>
2022-12-30 22:24:57 +0300

    redux config for upate user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -28,6 +28,10 @@ import {
     USER_DELETE_SUCCESS,
     USER_DELETE_FAIL,
 
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
+
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import axios from 'axios';
@@ -269,6 +273,46 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DELETE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
+
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const { data } = await axios.put(
+            `/api/users/update/${user._id}/`,
+            user,
+            {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            }
+        )
+
+        dispatch({
+            type:USER_UPDATE_SUCCESS,
+        })
+
+        dispatch({
+            type:USER_DETAILS_SUCCESS,
+            payload:data
+        })
+        
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
