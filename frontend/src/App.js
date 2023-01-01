926c609979e5d95ea2fe94cbe679ec9be6080eb1
slmnkankal <slmnknkl@gmail.com>
2023-01-01 23:49:32 +0300

    route for ProductEditScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -40,6 +41,7 @@ function App() {
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
 
             <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
           </Routes>
         </Container>
       </main>
