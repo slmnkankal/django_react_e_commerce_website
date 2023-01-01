cdd21778ee776ca683aba94c4b01cd1aba546957
slmnkankal <slmnknkl@gmail.com>
2023-01-01 15:24:36 +0300

    url for createProduct view

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -4,6 +4,8 @@ from base.views import product_views as views
 urlpatterns = [
 
     path('', views.getProducts, name="products" ),
+
+    path('create/', views.createProduct, name="product-create" ),
     path('<str:pk>/', views.getProduct, name="product" ),
 
     path('delete/<str:pk>/', views.deleteProduct, name="delete-product" ),
