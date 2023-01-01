484bcfbfe867250bbf18a902bf8542a82c4e02d3
slmnkankal <slmnknkl@gmail.com>
2023-01-01 17:48:18 +0300

    redux: createProduct action creator function

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -11,6 +11,11 @@ import {
     PRODUCT_DELETE_REQUEST,
     PRODUCT_DELETE_SUCCESS,
     PRODUCT_DELETE_FAIL,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL,
+
  } from '../constants/productConstants';
 
  export const listProducts = () => async (dispatch) => {
@@ -78,6 +83,40 @@ import {
     } catch (error) {
       dispatch({
         type: PRODUCT_DELETE_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
+
+
+ export const createProduct = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REQUEST,
+      });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const { data } = await axios.post(`/api/products/create/`,
+      {},
+       {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+  
+      dispatch({
+        type: PRODUCT_CREATE_SUCCESS,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_CREATE_FAIL,
         payload:
           error.response && error.response.data.detail
             ? error.response.data.detail
