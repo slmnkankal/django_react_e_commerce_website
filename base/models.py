2c716532b149d968b3d7d54a4c9da0989a1b33ef
slmnkankal <slmnknkl@gmail.com>
2023-01-03 22:12:34 +0300

    isDelevered>isDelivered (makemigrations & migrate)

--- base/models.py
+++ base/models.py
@@ -41,7 +41,7 @@ class Order(models.Model):
     totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
-    isDelevered = models.BooleanField(default=False)
+    isDelivered = models.BooleanField(default=False)
     deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
