bf0368b7e93360910133b63a3cd4d82e9f1251fa
slmnkankal <slmnknkl@gmail.com>
2023-01-03 21:33:05 +0300

    getOrders view

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -65,6 +65,13 @@ def getMyOrders(request):
     orders = user.order_set.all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
     
 
 @api_view(['GET'])
