9f381aef3004e512c39bb7b9407284d9a2f8b35b
slmnkankal <slmnknkl@gmail.com>
2023-01-03 21:33:29 +0300

    url for getOrders

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+    path('', views.getOrders, name='orders'),
     path('add/', views.addOrderItems, name='orders-add'),
     path('myorders/', views.getMyOrders, name='myorders'),
 
