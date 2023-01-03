0132eb51d830feb9a34f959bdf68b3c04083f3f5
slmnkankal <slmnknkl@gmail.com>
2023-01-03 11:41:31 +0300

    url for uploadImage

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.getProducts, name="products" ),
 
     path('create/', views.createProduct, name="product-create" ),
+    path('upload/', views.uploadImage, name="product-upload" ),
     path('<str:pk>/', views.getProduct, name="product" ),
 
     path('update/<str:pk>/', views.updateProduct, name="update-product" ),
