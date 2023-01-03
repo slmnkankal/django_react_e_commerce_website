8dc31ee70c0bdf4226035893e03f6acaf2577ccc
slmnkankal <slmnknkl@gmail.com>
2023-01-03 23:06:24 +0300

    updateOrderToDelivered view

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -101,3 +101,15 @@ def updateOrderToPaid(request, pk):
     order.save()
 
     return Response('Order was paid!')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isDelivered = True
+    order.deliveredAt = datetime.now()
+    order.save()
+
+    return Response('Order was delivered!')
