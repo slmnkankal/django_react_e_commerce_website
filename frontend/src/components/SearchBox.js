6c798949342621a54d570ab1b1857620ddaf5372
slmnkankal <slmnknkl@gmail.com>
2023-01-05 21:41:13 +0300

    SearchBox created & listProducts action modified

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function SearchBox() {
+  const [keyword, setKeyword] = useState("");
+
+  let navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword) {
+      navigate(`/?keyword=${keyword}`);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-success" className="p-2">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default SearchBox;
