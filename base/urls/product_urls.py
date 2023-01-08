b8f76bdf204e26a98fc5d69435790bbef05f6b84
slmnkankal <slmnknkl@gmail.com>
2023-01-08 22:23:55 +0300

    getTopProducts vew and url

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('upload/', views.uploadImage, name="product-upload" ),
 
     path('<str:pk>/reviews/', views.createProductReview, name="create-review" ),
+    path('top/', views.getTopProducts, name='top-products'),
     path('<str:pk>/', views.getProduct, name="product" ),
 
     path('update/<str:pk>/', views.updateProduct, name="update-product" ),
--- base/views/product_views.py
+++ base/views/product_views.py
@@ -19,7 +19,7 @@ def getProducts(request):
     products = Product.objects.filter(name__icontains=query)
 
     page = request.query_params.get('page')
-    paginator = Paginator(products, 2)
+    paginator = Paginator(products, 3)
 
     try:
         products = paginator.page(page)
@@ -35,6 +35,14 @@ def getProducts(request):
 
     serializer = ProductSerializer(products, many=True)
     return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
+
+
+@api_view(['GET'])
+def getTopProducts(request):
+    products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
 
 
 @api_view(['GET'])
